f76974448d4530b7990a56a6ee9b8e765958c7c4
Christian Whitehouse <christian.whitehouse@gmail.com>
2021-02-10 14:40:53 -0500

    try font display optional with better fallback fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,9 @@ module.exports = {
   darkMode: 'class',
   theme: {
     fontFamily: {
-      sans: ['IBM Plex Sans', 'sans-serif'],
-      serif: ['IBM Plex Serif', 'serif'],
-      mono: ['IBM Plex Mono', 'mono'],
+      sans: ['IBM Plex Sans', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      serif: ['IBM Plex Serif', 'ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'],
+      mono: ['IBM Plex Mono', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', "Liberation Mono", "Courier New", 'monospace'],
     },
     extend: {
       colors: {
