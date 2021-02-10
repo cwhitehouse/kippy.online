7f02eb88d09d82bdcf84ecf5310ef8b7310a7f29
Christian Whitehouse <christian.whitehouse@gmail.com>
2021-02-10 14:56:40 -0500

    favicons!

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,9 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
 
+  eleventyConfig.addWatchTarget("src/assets");
+  eleventyConfig.addPassthroughCopy("src/assets");
+
   eleventyConfig.addWatchTarget("tmp/styles/main.css");
   eleventyConfig.addPassthroughCopy({ "tmp/styles": "styles" });
 
