a68971ec98456370ed77284936718580fd8b87dc
Christian Whitehouse <christian.whitehouse@gmail.com>
2021-02-11 13:31:03 -0500

    differentiate between mouseover and click

--- src/scripts/components/page.js
+++ src/scripts/components/page.js
@@ -1,6 +1,7 @@
 export default {
   data() {
     return {
+      usedMouse: false,
       showingModal: false,
       systemDark: false,
     };
@@ -30,6 +31,25 @@ export default {
 
     updateSystemDark() {
       this.systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    },
+
+    mouseOverDarkMode() {
+      // this.usedMouse = true;
+      this.showingModal = true;
+    },
+
+    clickedDarkMode() {
+      console.log('clikceddarkmode!');
+
+      this.usedMouse = false;
+      this.showingModal = true;
+    },
+
+    setMode(mode) {
+      if (!this.usedMouse)
+        this.showingModal = false;
+
+      this.mode = mode;
     },
   },
 
