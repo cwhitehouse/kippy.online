495eb7158ee76f4545239e2334e0d674ccb5f9d5
Christian Whitehouse <christian.whitehouse@gmail.com>
2021-02-10 20:22:30 -0500

    fun new dark mode magic!

--- src/scripts/components/page.js
+++ src/scripts/components/page.js
@@ -1,10 +1,25 @@
 export default {
+  data() {
+    return {
+      showingModal: false,
+      systemDark: false,
+    };
+  },
+
   stored: {
-    darkMode: {
-      type: 'session',
-      defaultValue() {
-        return window.matchMedia('(prefers-color-scheme: dark)').matches;
-      },
+    mode: {
+      type: 'local',
+      defaultValue: 'auto',
+    },
+  },
+
+  computed: {
+    darkMode() {
+      const { mode, systemDark } = this;
+
+      if (mode === 'light') return false;
+      if (mode === 'dark') return true;
+      return systemDark;
     },
   },
 
@@ -12,5 +27,14 @@ export default {
     scrollToTop() {
       window.scrollTo(0, 0);
     },
-  }
+
+    updateSystemDark() {
+      this.systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    },
+  },
+
+  beforeConnected() {
+    this.updateSystemDark();
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', this.updateSystemDark.bind(this));
+  },
 };
