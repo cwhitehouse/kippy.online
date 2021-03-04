fdee2b657789a63701c72c646db2da67f5940b6b
Christian Whitehouse <christian.whitehouse@gmail.com>
2021-03-04 18:48:59 -0500

    add turbo!

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,18 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
 
+  eleventyConfig.setBrowserSyncConfig({
+    // scripts in body conflict with Turbolinks
+    snippetOptions: {
+      rule: {
+        match: /<\/head>/i,
+        fn: function(snippet, match) {
+          return snippet + match;
+        }
+      }
+    }
+  });
+
   eleventyConfig.addWatchTarget("src/assets");
   eleventyConfig.addPassthroughCopy("src/assets");
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
+import * as Turbo from '@hotwired/turbo';
 import { Vivere } from 'vivere';
 import Page from './components/page';
 
